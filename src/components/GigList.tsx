6c4814a7814d0e60953c662510cc3fd2cd0fb52f
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-07-31 06:10:54 +0300

    Update GigList.tsx
    Removed gigs dummy data

--- src/components/GigList.tsx
+++ src/components/GigList.tsx
@@ -11,66 +11,6 @@ export type GigType = {
   description: string,
 };
 
-// export const gigs: GigType[] = [
-//   {
-//     id: 1,
-//     title: "Cleaning the dog",
-//     price: 20000,
-//     location: "Nsambya",
-//     details: "All you are going to do is to wash three of my dogs",
-//     description:
-//       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil ullam libero est quaerat in animi officiis cumque ea! Sed veritatis odit fugit autem. Accusamus provident itaque nam impedit corporis eos! ",
-//   },
-//   {
-//     id: 2,
-//     title: "Walking the dog",
-//     price: 35000,
-//     location: "Muyenga",
-//     description:
-//       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil ullam libero est quaerat in animi officiis cumque ea! Sed veritatis odit fugit autem. Accusamus provident itaque nam impedit corporis eos! ",
-//   },
-//   {
-//     id: 3,
-//     title: "Sing for me",
-//     location: "Kawempe",
-//     price: 15000,
-//     description:
-//       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil ullam libero est quaerat in animi officiis cumque ea! Sed veritatis odit fugit autem. Accusamus provident itaque nam impedit corporis eos! ",
-//   },
-//   // {
-//   //   id: 4,
-//   //   title: "Cleaning the dig",
-//   //   description:
-//   //     "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil ullam libero est quaerat in animi officiis cumque ea! Sed veritatis odit fugit autem. Accusamus provident itaque nam impedit corporis eos! ",
-//   // },
-//   // {
-//   //   id: 5,
-//   //   title: "Slashing the compound",
-//   //   description:
-//   //     "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil ullam libero est quaerat in animi officiis cumque ea! Sed veritatis odit fugit autem. Accusamus provident itaque nam impedit corporis eos! ",
-//   // },
-//   // {
-//   //   id: 6,
-//   //   title: "Cleaning the pavers",
-//   //   description:
-//   //     "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil ullam libero est quaerat in animi officiis cumque ea! Sed veritatis odit fugit autem. Accusamus provident itaque nam impedit corporis eos! ",
-//   // },
-//   // {
-//   //   id: 7,
-//   //   title: "Washing the car",
-//   //   description:
-//   //     "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil ullam libero est quaerat in animi officiis cumque ea! Sed veritatis odit fugit autem. Accusamus provident itaque nam impedit corporis eos! ",
-//   // },
-//   // {
-//   //   id: 8,
-//   //   title: "Dance for cash",
-//   //   description:
-//   //     "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil ullam libero est quaerat in animi officiis cumque ea! Sed veritatis odit fugit autem. Accusamus provident itaque nam impedit corporis eos! ",
-//   // },
-// ];
-
-
-
 const GigList = () => {
   const [gigs, setGigs] = useState<GigType[]>([])
   async function fetchGigs() {
