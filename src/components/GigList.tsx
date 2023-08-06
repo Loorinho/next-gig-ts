b20b850f60bb706bd5ddfd3612aa9126f9ed9e72
Jacobson Loor <72121391+Loorinho@users.noreply.github.com>
2023-08-06 16:26:14 +0300

    Update GigList.tsx
    Added gig creation button and dialog

--- src/components/GigList.tsx
+++ src/components/GigList.tsx
@@ -5,6 +5,10 @@ const GigList = () => {
   const gigs = useGigStore(state=>state.gigs)
  
   return (
+    <>
+    <button className="px-4 py-2 rounded bg-blue-600 text-white m-3">
+      Add Gig
+    </button>  
     <div className="m-5 flex justify-center gap-2 flex-wrap ">
       {gigs?.map((gig) => (
         <div key={gig.id}>
@@ -12,6 +16,10 @@ const GigList = () => {
         </div>
       ))}
     </div>
+     <dialog open className="create_gig_dialog">
+       <p>Create a gog</p>
+     </dialog> 
+    </>
   );
 };
 
